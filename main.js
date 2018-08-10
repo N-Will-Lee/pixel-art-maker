2b0cca5052a41bad2d91d2e03b8c76c236ed8424
Nicholas Lee <nicholaslee@Nicholass-MacBook-Air-2.local>
2018-08-10 13:14:04 -0600

    cleaned up code by removing console.logs, unused functions, and unused variables

--- main.js
+++ main.js
@@ -1,65 +1,43 @@
 document.addEventListener('DOMContentLoaded', function()    {
 
+    var gridSize = 10;
+    var main = document.querySelector('main');
+    const colors = document.getElementById('colors');
+    var selectedColor = 'black';
+    var sColorBox = document.getElementById('selectedColor');
 
-var gridSize = 10;
-var main = document.querySelector('main');
-var body = document.querySelector('body');
-const colors = document.getElementById('colors');
-var selectedColor = 'black';
-var sColorBox = document.getElementById('selectedColor');
-var canvasButton = document.getElementById('canvasButton');
-
-createGrid(gridSize);
-
-main.addEventListener('click',function(event){
-    if ((event.target.className == "row") || (event.target.className == "main"))  {
-        return;
+    createGrid(gridSize);
+    
+    main.addEventListener('click',function(event){
+        if ((event.target.className == "row") || (event.target.className == "main"))  {
+            return;
+        }
+        var cell = event.target;
+        cell.style.backgroundColor = selectedColor;
+    })
+    colors.addEventListener('click', function(event)    {
+        const select = event.target;
+        selectedColor = select.style.backgroundColor;
+        sColorBox.style.backgroundColor = selectedColor;
+    })
+    function createRow() {
+        const div = document.createElement('div');
+        div.classList.add('row');
+        return div;
     }
-    var cell = event.target;
-    console.log(cell);
-    cell.style.backgroundColor = selectedColor;
-})
-
-colors.addEventListener('click', function(event)    {
-    const select = event.target;
-    console.log(select);
-    selectedColor = select.style.backgroundColor;
-    console.log(selectedColor)
-    sColorBox.style.backgroundColor = selectedColor;
-})
-
-// canvasButton.addEventListener('click',function(event)   {
-//     const inputNumber = document.getElementById('inputValue');
-//     gridSize = parseInt(inputNumber.value);
-//     console.log(gridSize);
-    
-//     // body.removeChild('main');    
-    
-//     createGrid(gridSize);
-// })
-
-function createRow() {
-    const div = document.createElement('div');
-    div.classList.add('row');
-    return div;
-}
-
-function appendCellsTo(row, numberToCreate) {
-    for (let j=0; j < numberToCreate; j++)  {
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-        row.appendChild(cell);
+    function appendCellsTo(row, numberToCreate) {
+        for (let j=0; j < numberToCreate; j++)  {
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            row.appendChild(cell);
+        }
     }
-}
-function createGrid(newGridSize){
-    main.removeChild;
-    
-    for (let i=0; i < newGridSize; i++)    {
-        const row = createRow()
-        appendCellsTo(row, gridSize)
-        main.append(row)
-    }   
-}
-
-
+    function createGrid(newGridSize){
+        main.removeChild;       
+        for (let i=0; i < newGridSize; i++)    {
+            const row = createRow()
+            appendCellsTo(row, gridSize)
+            main.append(row)
+        }   
+    }
 })
